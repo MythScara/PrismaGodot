c21b47ac9daf83a304d412dbc827e91dda3fd1a3
Amith Bhaskara <themythicmango2@gmail.com>
2024-04-21 19:02:03 -0400

    Technique Update
    - Modified technique change in inventory with contextual menu
    - Changed button style and text style

--- script/inventory/techniques.gd
+++ script/inventory/techniques.gd
@@ -4,11 +4,30 @@ extends Control
 @onready var special_button = $Special
 @onready var super_button = $Super
 
+var ItemInfo = preload("res://object/item_info.tscn")
+var cur_selected = null
+var current_info = null
+
+var buttonstyle = preload("res://object/standardbutton.tscn")
+var textstyle = preload("res://object/standard_richtext.tscn")
+var selected_button = null
+signal button_pressed(text)
+
 func _ready():
 	PlayerStats.connect("pause_game", Callable(self, "update_techniques"))
 	update_techniques()
 
+func _on_Button_pressed(new_button : Button):
+	# Find the pressed button
+	if selected_button != null and selected_button != new_button:
+		selected_button.set_pressed(false)
+	
+	selected_button = new_button
+	new_button.set_pressed(true)
+	emit_signal("button_pressed", new_button.text)
+
 func update_techniques():
+	print(PlayerInventory.current_inventory["Techniques"])
 	var index = 0
 	for tech in PlayerInventory.current_inventory["Techniques"]:
 		var text = "Technique Available"
@@ -41,16 +60,63 @@ func replace_technique(button, t_name, t_key, technique, method):
 		print("Technique Change Failed")
 		PlayerInterface.clear_selection()
 
+func display_info(button, key_name = null, input = null):
+	if key_name == cur_selected:
+		return
+	
+	if current_info != null:
+		current_info.queue_free()
+	
+	var specialist = PlayerStats.load_specialist(input["Name"])
+	var info = specialist.specialist_info
+	cur_selected = key_name
+	
+	var t_name = null
+	var t_type = null
+	
+	match input["Technique"]:
+		"skill_technique":
+			t_name = "Technique 1"
+			t_type = "Skill"
+		"special_technique":
+			t_name = "Technique 2"
+			t_type = "Special"
+		"super_technique":
+			t_name = "Technique 3"
+			t_type = "Super"
+			
+	if button != null:
+		current_info = ItemInfo.instantiate()
+		PlayerInterface.information_field.add_child(current_info)
+		current_info.get_node("Name").text = key_name
+		var item_type = "Technique"
+		current_info.get_node("ItemType").text = item_type
+		current_info.get_node("ItemTier").text = "Standard"
+		current_info.get_node("ItemQuality").text = "Duration: " + str(info[t_name]["TD"])
+		current_info.get_node("ItemElement").text = "Cooldown: " + str(info[t_name]["TC"])
+		current_info.get_node("ItemImage").texture = load("res://asset/technique_icons/" + key_name.to_lower() + ".png")
+		current_info.get_node("ItemExtra").text = ""
+		current_info.get_node("EquipButton").connect("pressed", Callable(self, "replace_technique").bind(button, key_name, input, input["Name"], input["Technique"]))
+		current_info.get_node("EquipButton").text = "EQUIP " + button.to_upper()
+		
+		var key_text = textstyle.instantiate()
+		current_info.get_node("Scroll/StatBar").add_child(key_text)
+		key_text.bbcode_text = info[t_name][t_type]
+
 func display_technique(button):
 	PlayerInterface.clear_selection()
 	for key in PlayerInventory.equip_inventory["Techniques"].keys():
-		var option = Button.new()
+		var option = buttonstyle.instantiate()
 		var input = PlayerInventory.equip_inventory["Techniques"]
 		option.text = key
 		option.add_theme_font_size_override("font_size", 20)
 		option.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-		option.connect("pressed", Callable(self, "replace_technique").bind(button, key, input[key], input[key]["Name"], input[key]["Technique"]))
+		option.connect("pressed", Callable(self, "display_info").bind(button, key, input[key]))
+		option.pressed.connect(_on_Button_pressed.bind(option))
 		PlayerInterface.selection_field.add_child(option)
+		
+		if key in PlayerInventory.current_inventory["Techniques"][0].keys():
+			_on_Button_pressed(option)
 
 func _on_skill_pressed():
 	display_technique("Skill")
