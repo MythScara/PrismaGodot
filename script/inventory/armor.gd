d6da4f6e06bda0e911e09dba36cb4f2edbca8e97
Amith Bhaskara <themythicmango2@gmail.com>
2024-05-03 13:50:59 -0400

    Update armor.gd

--- script/inventory/armor.gd
+++ script/inventory/armor.gd
@@ -22,5 +22,28 @@ func _on_Button_pressed(new_button : Button):
 	new_button.set_pressed(true)
 	emit_signal("button_pressed", new_button.text)
 
+func image_set(type):
+		if type != null and PlayerInventory.current_inventory[type][0] != null:
+			var image = PlayerInventory.current_inventory[type][0].keys()[0]
+	
+			match type:
+				"Chest Armor":
+					chest.texture = load("res://asset/armor_icons/" + image.to_lower + ".png")
+				"Pad Armor":
+					pad.texture = load("res://asset/armor_icons/" + image.to_lower + ".png")
+				"Belt Armor":
+					belt.texture = load("res://asset/armor_icons/" + image.to_lower + ".png")
+				"Body Armor":
+					body.texture = load("res://asset/armor_icons/" + image.to_lower + ".png")
+				"Outift":
+					outfit.texture = load("res://asset/armor_icons/" + image.to_lower + ".png")
+
+func update_values():
+	image_set("Body Armor")
+	image_set("Pad Armor")
+	image_set("Belt Armor")
+	image_set("Body Armor")
+	image_set("Outfit")
+
 func _ready():
-	pass # Replace with function body.
+	update_values()
