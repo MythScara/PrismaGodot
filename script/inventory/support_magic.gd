81c78a1fc1cdb270d4c1ff8bc831120e5a490bae
Amith Bhaskara <themythicmango2@gmail.com>
2024-05-07 23:43:09 -0400

    Minor Update

--- script/inventory/support_magic.gd
+++ script/inventory/support_magic.gd
@@ -1,4 +1,20 @@
 extends Control
 
+var ItemInfo = preload("res://object/item_info.tscn")
+var current_info = null
+
+var buttonstyle = preload("res://object/standardbutton.tscn")
+var selected_button = null
+signal button_pressed(text)
+
+func _on_Button_pressed(new_button : Button):
+	# Find the pressed button
+	if selected_button != null and selected_button != new_button:
+		selected_button.set_pressed(false)
+	
+	selected_button = new_button
+	new_button.set_pressed(true)
+	emit_signal("button_pressed", new_button.text)
+
 func _ready():
 	pass
