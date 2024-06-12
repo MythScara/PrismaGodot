bb565a5dfb27654771e9f25dc33ea9ddb1885d4e
Amith Bhaskara <themythicmango2@gmail.com>
2024-06-12 19:48:51 -0400

    Display Update

--- script/object/item_spec.gd
+++ script/object/item_spec.gd
@@ -7,5 +7,23 @@ extends Button
 @onready var image = $Image
 @onready var values = {}
 
+var current_equip
+
+func _ready():
+	PlayerStats.connect("pause_game", Callable(self, "check_value"))
+
 func _pressed():
 	PlayerInterface.set_display(type, image, item_name, values, slot)
+
+func check_value():
+	current_equip = PlayerInventory.current_inventory[type][slot]
+	if current_equip != null:
+		item_name = current_equip.keys()[0]
+		values = current_equip[item_name]
+		var path = "res://asset/" + type.to_lower() + "_icons/" + item_name.to_lower() + ".png"
+		if ResourceLoader.exists(path):
+			pass
+		else:
+			path = "res://asset/hud_icons/locked_icon.png"
+		
+		image.texture = load(path)
